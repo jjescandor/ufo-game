cddc156ed8763e5c08f372e21bcd464f43b5286b
JJ Escandor <jj.p.escandor@gmail.com>
2021-10-31 23:48:25 -0400

    added spaceship

---
+++ js/object.js
@@ -0,0 +1,14 @@
+// Objects
+function Objects() {
+
+};
+
+Objects.prototype.spaceship = function (x, y, spaceship_image) {
+    this.x = x;
+    this.y = y;
+    this.width = 34;
+    this.height = 28;
+    this.spaceship_image = spaceship_image;
+    this.spaceship_image.src = "img/ship.png";
+    return this;
+}
--- js/positionInGame.js
+++ js/positionInGame.js
@@ -1,12 +1,28 @@
 // --- positionInGame --- //
 
 function InGamePosition(settings, level) {
+    this.settings = settings;
+    this.level = level;
+    this.oject = null;
+    this.spaceship = null;
+}
+
+InGamePosition.prototype.update = function (play) {
+
+}
+
+InGamePosition.prototype.entry = function (play) {
+    this.spaceship_image = new Image();
+    this.object = new Objects();
+    this.spaceship = this.object.spaceship((play.width / 2), play.playBounderies.bottom, this.spaceship_image)
+}
+
+InGamePosition.prototype.keyDown = function (play) {
 
 }
 
 InGamePosition.prototype.draw = function (play) {
     ctx.clearRect(0, 0, play.width, play.height);
-    ctx.font = '40px Calibri';
-    ctx.fillStyle = '#D7DF01';
-    ctx.fillText('We are in the In Game Position', play.width / 2, play.height / 2);
+    ctx.drawImage(this.spaceship_image, this.spaceship.x - (this.spaceship.width / 2), this.spaceship.y - (this.spaceship.height - 2));
 }
+
